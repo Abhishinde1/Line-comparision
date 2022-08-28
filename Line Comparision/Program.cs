97bda8a09f44f92b03661a742514689e3fed18dd
AbhiShinde1 <abhishinde572@gmail.com>
2022-08-28 15:25:56 +0530

    [Abhi] UsingCartesianSystem

---
+++ Line Comparision/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Line_Comparision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line Comparison Program");
+            Console.WriteLine("console option \n 1. Enter the Points ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    UC1LineComparison.GetDistance();
+                    break;
+            }
+        }
+    }
+}
---
+++ Line Comparision/UC1LineComparison.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparision
+{
+    internal class UC1LineComparison
+    {
+        public static void GetDistance()
+        {
+
+            Console.WriteLine("enter the value of x1 ");
+            double x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the value of x2 ");
+            double x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the value of y1 ");
+            double y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the value of y2 ");
+            double y2 = Convert.ToInt32(Console.ReadLine());
+
+            double Result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Result is " + Result);
+        }
+    }
+}
